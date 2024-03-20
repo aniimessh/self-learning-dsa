dcc4f6b2d7cae2d8488d5db139be8cb0876b382f
aniimessh <animeshgupta2.002@gmail.com>
2024-03-20 22:23:24 +0530

    find middle in stack

--- stacks/reverseString/main.cpp
+++ stacks/reverseString/main.cpp
@@ -1,15 +1,42 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
-int main(){
-    string str = "Animesh";
-    stack<char> name;
-    for(int i=0;i<str.length();i++){
-        name.push(str[i]);
+
+void findMiddle(stack<int> &st, int &size)
+{
+    if (st.size() == size + 1)
+    {
+        cout << "Middle element is " << st.top() << " \n";
+        return;
     }
-    while(!name.empty()){
-        cout<<name.top()<<" ";
-        name.pop();
-    }
-    cout<<"\n";
+    int temp = st.top();
+    st.pop();
+    findMiddle(st, size);
+    st.push(temp);
+}
+
+int main()
+{
+    stack<int> st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+    st.push(60);
+    st.push(70);
+    int middle = st.size() / 2;
+    findMiddle(st, middle);
+
+    // string str = "Animesh";
+    // stack<char> name;
+    // for(int i=0;i<str.length();i++){
+    //     name.push(str[i]);
+    // }
+    // while(!name.empty()){
+    //     cout<<name.top()<<" ";
+    //     name.pop();
+    // }
+    // cout<<"\n";
+    return 0;
 }
